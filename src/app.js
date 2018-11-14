3d8ee08d0da749b65e2776266b23ae65b5294276
Jorge <jazmitia_10@hotmail.com>
2018-11-14 02:03:28 -0600

    WEBPACK
    webpack used for loading/creating html custom template, and webpackk used for loading css, sass for css

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("hello from app.js");
+const css = require('./app.scss');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Hello World!';
+console.log('My Minimal React Webpack Babel Setup');
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,48 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require ('webpack');
 module.exports =  {
 
     entry: './src/app.js',
     output: {
-        filename:'./app.bundle.js' 
-    }
+        path: __dirname + '/dist',
+        filename: 'app.bundle.js'
+      } ,
+      module:{
+        rules: [
+          {
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+          },
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+    ]
+      },
+      devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        hot:true,
+        stats:"errors-only"
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Project Demo',
+          minify:{
+            collapseWhitespace: true
+          },
+          template: './src/test.html'
+
+        }),
+        new webpack.HotModuleReplacementPlugin()
+        //new webpack.NameModulesPlugin()
+
+      ]
      
 }
